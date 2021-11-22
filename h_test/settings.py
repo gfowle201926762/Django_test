82b713526e2deaa385cad75095176d4d9413250c
Gus Fowle <61168750+gfowle201926762@users.noreply.github.com>
2021-11-22 23:11:39 +0000

    Update settings.py

--- h_test/settings.py
+++ h_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cq-yystggdpl@i#=u@bo4x#7#gi_lkue6bka_7vgs^irb@v&)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fluenco.herokuapp.com']
 
 
 # Application definition
